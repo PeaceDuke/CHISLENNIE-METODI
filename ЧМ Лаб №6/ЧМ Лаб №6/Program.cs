a90db3a01baf99b7fd951eb68e703bb3bc38171a
PeaceDuke <PeaceDuke@users.noreply.github.com>
2017-11-17 21:36:19 +0500

    Rtr

---
+++ ЧМ Лаб №6/ЧМ Лаб №6/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Math;
+
+namespace ЧМ_Лаб__6
+{
+    class Program
+    {
+        private const int N = 100000, Var = 17;
+        private const double A = 1, B = 2;
+        private const double Delta = (B - A) / N;
+        private static readonly StreamWriter OutFile = new StreamWriter("out.txt");
+
+        private static double Func(double x)
+        {
+            switch (Var)
+            {
+                case 16: return Atan(x) + 1 / x / x;
+                case 17: return Exp(x) + x + 1;
+                default: return 0;
+            }
+        }
+
+        private static double RealValue
+        {
+            get
+            {
+                switch (Var)
+                {
+                    case 16: return 1.470753906253655163826706468653192823365752331004957604213;
+                    case 17: return 7.170774270471604991870139989222345315423068476851887749120;
+                    default: return 0;
+                }
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            double t = CalcTrapezeIntegral();
+            PrintStr("Метод трапеции");
+            PrintStr("Приближенное значение: " + t);
+            PrintStr("Погрешность: " + Abs(t - RealValue));
+            t = CalcSimpsonIntegral();
+            PrintStr("Метод Симпсона");
+            PrintStr("Приближенное значение: " + t);
+            PrintStr("Погрешность: " + Abs(t - RealValue));
+            t = CalcGaussIntegral();
+            PrintStr("Метод Гаусса(трехточечный)");
+            PrintStr("Приближенное значение: " + t);
+            PrintStr("Погрешность: " + Abs(t - RealValue));
+        }
+
+        private static double CalcTrapezeIntegral()
+        {
+            double sum = 0, a, b;
+            for(int i = 0; i < N; i++)
+            {
+                a = A + Delta * i;
+                b = A + Delta * (i + 1);
+                sum += (b - a) * (Func(a) + Func(b)) / 2;
+            }
+            return sum;
+        }
+
+        private static double CalcSimpsonIntegral()
+        {
+            double sum = 0, a, b;
+            for (int i = 0; i < N; i++)
+            {
+                a = A + Delta * i;
+                b = A + Delta * (i + 1);
+                sum += (b - a) * (Func(a) + Func(b) + 4 * Func((a + b) / 2)) / 6;
+            }
+            return sum;
+        }
+
+        private static double CalcGaussIntegral()
+        {
+            double sum = 0, a, b, p;
+            for (int i = 0; i < N; i++)
+            {
+                a = A + Delta * i;
+                b = A + Delta * (i + 1);
+                sum += (b - a) * (5.0 / 9.0 * Func(0.5 *(a + b + (b - a) * -Sqrt(3.0 / 5.0))) + 8.0 / 9.0 * Func(0.5 * (a + b + (b - a))) + 5.0 / 9.0 * Func(0.5 * (a + b + (b - a) * Sqrt(3.0 / 5.0)))) / 2;
+            }
+            return sum;
+        }
+
+        private static void PrintStr(string str)
+        {
+            OutFile.WriteLine(str);
+            Console.WriteLine(str);
+        }
+
+        private static void PrintStr()
+        {
+            OutFile.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
